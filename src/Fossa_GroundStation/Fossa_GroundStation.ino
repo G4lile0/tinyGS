7761ef7eb947f7a65f471e2e582ca1edbb83e39e
G4lile0 <galile0@hotmail.com>
2019-12-22 20:41:38 +0100

    updated initial description

--- src/Fossa_GroundStation/Fossa_GroundStation.ino
+++ src/Fossa_GroundStation/Fossa_GroundStation.ino
@@ -1,8 +1,32 @@
 /*
-    Based on theRadioLib SX127x Receive with Interrupts Example
-    For full API reference, see the GitHub Pages
-     https://jgromes.github.io/RadioLib/
-     
+The aim of this project is to create an open network of ground stations for the Fossa Satellites distributed all over the world and connected through Internet.
+This project is based on ESP32 boards and is compatible with sx126x and sx127x you can build you own board using one of these modules but most of us use a development 
+board like the ones listed in the Supported boards section.
+The developers of this project have no relation with the Fossa team in charge of the mission, we are passionate about space and created this project to be able to track
+and use the satellites as well as supporting the mission.
+
+Supported boards
+    Heltec WiFi LoRa 32 V1 (433MHz SX1278)
+    Heltec WiFi LoRa 32 V2 (433MHz SX1278) 
+    TTGO LoRa32 V1 (433MHz SX1278)
+    TTGO LoRa32 V2 (433MHz SX1278)
+
+Supported modules
+    sx126x
+    sx127x
+
+    World Map with active Ground Stations and satellite stimated possition 
+    Main community chat: https://t.me/joinchat/DmYSElZahiJGwHX6jCzB3Q
+      In order to onfigure your Ground Station please open a private chat to get your credentials https://t.me/fossa_updates_bot
+    Data channel (station status and received packets): https://t.me/FOSSASAT_DATA
+    Test channel (simulator packets received by test groundstations): https://t.me/FOSSASAT_TEST
+
+    Developers:
+              @gmag12       https://twitter.com/gmag12
+              @dev_4m1g0    https://twitter.com/dev_4m1g0
+              @g4lile0       https://twitter.com/G4lile0
+
+    LICENSE 	GNU General Public License v3.0 
   */
 
 // include the library
