4b643d81409ca41cd83c86079392528fcbe2c3f1
4m1g0 <dev.4m1g0@gmail.com>
2019-12-26 00:24:08 +0100

    Remove debug

--- FossaGroundStationProject/lib/IotWebConf/src/IotWebConf.h
+++ FossaGroundStationProject/lib/IotWebConf/src/IotWebConf.h
@@ -47,7 +47,7 @@
 #define IOTWEBCONF_DEBUG_TO_SERIAL
 
 // -- Logs passwords to Serial if enabled.
-#define IOTWEBCONF_DEBUG_PWD_TO_SERIAL
+//#define IOTWEBCONF_DEBUG_PWD_TO_SERIAL
 
 // -- Helper define for serial debug
 #ifdef IOTWEBCONF_DEBUG_TO_SERIAL
