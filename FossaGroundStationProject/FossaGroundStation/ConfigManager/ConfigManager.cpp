7d87565a934e9e33fb552fdc8120e998771b9558
4m1g0 <dev.4m1g0@gmail.com>
2019-12-26 00:22:06 +0100

    Add all custom parameters including option select

--- FossaGroundStationProject/FossaGroundStation/ConfigManager/ConfigManager.cpp
+++ FossaGroundStationProject/FossaGroundStation/ConfigManager/ConfigManager.cpp
@@ -13,12 +13,26 @@ ConfigManager::ConfigManager()
   setHtmlFormatProvider(&gsConfigHtmlFormatProvider);
   formValidatorStd = std::bind(&ConfigManager::formValidator, this);
   setFormValidator(formValidatorStd);
+  skipApStartup();
+  
 
-  addParameter(&stringParam);
-  addParameter(&separator1);
-  addParameter(&intParam);
-  addParameter(&separator2);
-  addParameter(&floatParam);
+  // Customize own parameters
+  getThingNameParameter()->label = "GroundStation Name";
+  getApPasswordParameter()->label = "GroundStation password";
+
+  
+
+
+  addParameter(&latitudeParam);
+  addParameter(&longitudeParam);
+  addParameter(&tzParam);
+  addParameter(&mqttSeparator);
+  addParameter(&mqttServerParam);
+  addParameter(&mqttPortParam);
+  addParameter(&mqttUserParam);
+  addParameter(&mqttPassParam);
+  addParameter(&separatorBoard);
+  addParameter(&boardParam);
 }
 
 
@@ -69,12 +83,38 @@ bool ConfigManager::formValidator()
   Serial.println("Validating form.");
   boolean valid = true;
 
-  int l = server.arg(stringParam.getId()).length();
+  int l = 0;//server.arg(stringParam.getId()).length();
   if (l < 3)
   {
-    stringParam.errorMessage = "Please provide at least 3 characters for this test!";
+    //stringParam.errorMessage = "Please provide at least 3 characters for this test!";
     valid = false;
   }
 
   return true;
 }
+
+void ConfigManager::resetAPConfig() {
+  getWifiSsidParameter()->valueBuffer[0] = '\0';
+  getWifiPasswordParameter()->valueBuffer[0] = '\0';
+  strncpy(getApPasswordParameter()->valueBuffer, initialApPassword, IOTWEBCONF_WORD_LEN);
+  strncpy(getThingNameParameter()->valueBuffer, thingName, IOTWEBCONF_WORD_LEN);
+  strncpy(getThingName(), thingName, IOTWEBCONF_WORD_LEN);
+  configSave();
+}
+
+void ConfigManager::resetAllConfig(){
+  getWifiSsidParameter()->valueBuffer[0] = '\0';
+  getWifiPasswordParameter()->valueBuffer[0] = '\0';
+  strncpy(getApPasswordParameter()->valueBuffer, initialApPassword, IOTWEBCONF_WORD_LEN);
+  strncpy(getThingNameParameter()->valueBuffer, thingName, IOTWEBCONF_WORD_LEN);
+  strncpy(getThingName(), thingName, IOTWEBCONF_WORD_LEN);
+  strncpy(mqttPortParam.valueBuffer, MQTT_DEFAULT_PORT, MQTT_PORT_LENGTH);
+  strncpy(mqttServerParam.valueBuffer, MQTT_DEFAULT_SERVER, MQTT_SERVER_LENGTH);
+  mqttUserParam.valueBuffer[0] = '\0';
+  mqttPassParam.valueBuffer[0] = '\0';
+}
+
+/*boolean ConfigManager::init() {
+  IotWebConf::init();
+  resetAllConfig();
+}*/
--- FossaGroundStationProject/FossaGroundStation/ConfigManager/ConfigManager.h
+++ FossaGroundStationProject/FossaGroundStation/ConfigManager/ConfigManager.h
@@ -2,12 +2,15 @@
 #include "logos.h"
 
 constexpr auto STATION_NAME_LENGTH = 21;
+constexpr auto COORDINATE_LENGTH = 10;
 constexpr auto MQTT_SERVER_LENGTH = 31;
+constexpr auto MQTT_PORT_LENGTH = 6;
 constexpr auto MQTT_USER_LENGTH = 31;
 constexpr auto MQTT_PASS_LENGTH = 31;
 constexpr auto SSID_LENGTH = 32;
 constexpr auto PASS_LENGTH = 64;
 constexpr auto TZ_LENGTH = 40;
+constexpr auto BOARD_LENGTH = 3;
 
 constexpr auto ROOT_URL = "/";
 constexpr auto CONFIG_URL = "/config";
@@ -17,9 +20,13 @@ constexpr auto UPDATE_URL = "/firmware";
 const char TITLE_TEXT[] PROGMEM = "FOSSA Ground Satation Configuration";
 
 
-constexpr auto thingName = "GroundStation";
+constexpr auto thingName = "test_GroundStation";
 constexpr auto initialApPassword = "";
 constexpr auto configVersion = "v0.0.1";
+constexpr auto MQTT_DEFAULT_SERVER = "fossa.apaluba.com";
+constexpr auto MQTT_DEFAULT_PORT = "8883";
+
+#include "htmlOptions.h"
 
 class GSConfigHtmlFormatProvider : public IotWebConfHtmlFormatProvider
 {
@@ -33,8 +40,21 @@ protected:
   String getBodyInner() override
   {
     return
-      //String(FPSTR(CUSTOMHTML_BODY_INNER)) +
+      String(FPSTR(LOGO)) +
       IotWebConfHtmlFormatProvider::getBodyInner();
+  }
+
+  String getFormParam(const char* type) override
+  {
+    if (!strcmp(type, "TZ")) {
+      return FPSTR(IOTWEBCONF_HTML_FORM_TZ_PARAM);
+    }
+
+    if (!strcmp(type, "board")) {
+      return FPSTR(IOTWEBCONF_HTML_FORM_BOARD_PARAM);
+    }
+
+    return IotWebConfHtmlFormatProvider::getFormParam(type);
   }
 };
 
@@ -43,29 +63,41 @@ class ConfigManager : public IotWebConf
 {
 public:
   ConfigManager();
-
+  void resetAPConfig();
+  void resetAllConfig();
+  // boolean init();
 
 private:
   void handleRoot();
   void handleDashboard();
   bool formValidator();
+  
   std::function<boolean()> formValidatorStd;
   DNSServer dnsServer;
   WebServer server;
   HTTPUpdateServer httpUpdater;
   GSConfigHtmlFormatProvider gsConfigHtmlFormatProvider;
 
-#define STRING_LEN 128
-#define NUMBER_LEN 32
+  char latitude[COORDINATE_LENGTH] = "";
+  char longitude[COORDINATE_LENGTH] = "";
+  char tz[TZ_LENGTH] = "";
+  char mqttServer[MQTT_SERVER_LENGTH] = "";
+  char mqttPort[MQTT_PORT_LENGTH] = "";
+  char mqttUser[MQTT_USER_LENGTH] = "";
+  char mqttPass[MQTT_PASS_LENGTH] = "";
+  char board[BOARD_LENGTH] = "";
 
-  char stringParamValue[STRING_LEN];
-  char intParamValue[NUMBER_LEN];
-  char floatParamValue[NUMBER_LEN];
-  IotWebConfParameter stringParam = IotWebConfParameter("String param", "stringParam", stringParamValue, STRING_LEN);
-  IotWebConfSeparator separator1 = IotWebConfSeparator();
-  IotWebConfParameter intParam = IotWebConfParameter("Int param", "intParam", intParamValue, NUMBER_LEN, "number", "1..100", NULL, "min='1' max='100' step='1'");
-  // -- We can add a legend to the separator
-  IotWebConfSeparator separator2 = IotWebConfSeparator("Calibration factor");
-  IotWebConfParameter floatParam = IotWebConfParameter("Float param", "floatParam", floatParamValue, NUMBER_LEN, "number", "e.g. 23.4", NULL, "step='0.1'");
+  IotWebConfParameter latitudeParam = IotWebConfParameter("Latitude (will be public)", "lat", latitude, COORDINATE_LENGTH, "number", NULL, NULL, "required min='-180' max='180' step='0.001'");
+  IotWebConfParameter longitudeParam = IotWebConfParameter("Longitude (will be public)", "lng", longitude, COORDINATE_LENGTH, "number", NULL, NULL, "required min='-180' max='180' step='0.001'");
+  IotWebConfParameter tzParam = IotWebConfParameter("Time Zone", "tz", tz, TZ_LENGTH, "TZ", NULL, NULL);
+
+  IotWebConfSeparator mqttSeparator = IotWebConfSeparator("Mqtt credentials (get them <a href='https://t.me/joinchat/DmYSElZahiJGwHX6jCzB3Q'>here</a>)");
+  IotWebConfParameter mqttServerParam = IotWebConfParameter("Server address", "mqtt_server", mqttServer, MQTT_SERVER_LENGTH, "text", NULL, MQTT_DEFAULT_SERVER, "required type=\"text\" maxlength=30");
+  IotWebConfParameter mqttPortParam = IotWebConfParameter("Server Port", "mqtt_port", mqttPort, MQTT_PORT_LENGTH, "number", NULL, MQTT_DEFAULT_PORT, "required type=\"number\" min=\"0\" max=\"65536\" step=\"1\"");
+  IotWebConfParameter mqttUserParam = IotWebConfParameter("MQTT Username", "mqtt_user", mqttUser, MQTT_USER_LENGTH, "text", NULL, NULL, "required type=\"text\" maxlength=30");
+  IotWebConfParameter mqttPassParam = IotWebConfParameter("MQTT Password", "mqtt_pass", mqttPass, MQTT_PASS_LENGTH, "text", NULL, NULL, "required type=\"text\" maxlength=30");
+
+  IotWebConfSeparator separatorBoard = IotWebConfSeparator("Board config");
+  IotWebConfParameter boardParam = IotWebConfParameter("Board type", "board", board, BOARD_LENGTH, "board", NULL, NULL);
 };
 
