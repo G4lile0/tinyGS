fe0ffe5c6d3af54ee1045312acc949c4df7d2dff
4m1g0 <dev.4m1g0@gmail.com>
2019-12-30 03:13:30 +0100

    Fix typo

--- FossaGroundStation/src/ArduinoOTA/ArduinoOTA.cpp
+++ FossaGroundStation/src/ArduinoOTA/ArduinoOTA.cpp
@@ -1,7 +1,7 @@
 /*
   ArduinoOTA.cpp - functions to handle Arduino OTA Update
   
-  Copyright (C) 2020 @G4lile0, @gmag12 y @dev_4m1g0
+  Copyright (C) 2020 @G4lile0, @gmag12 and @dev_4m1g0
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
--- FossaGroundStation/src/Mqtt/esp32_mqtt_client.cpp
+++ FossaGroundStation/src/Mqtt/esp32_mqtt_client.cpp
@@ -1,7 +1,7 @@
 /*
   esp32_mqtt_client.cpp - MQTT connection class
   
-  Copyright (C) 2020 @G4lile0, @gmag12 y @dev_4m1g0
+  Copyright (C) 2020 @G4lile0, @gmag12 and @dev_4m1g0
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
