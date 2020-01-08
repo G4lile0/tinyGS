c3942faf0d7d234692c2614814e5388d1699de64
4m1g0 <dev.4m1g0@gmail.com>
2020-01-08 22:33:23 +0100

    Increase version number

--- FossaGroundStation/src/Status.h
+++ FossaGroundStation/src/Status.h
@@ -42,7 +42,7 @@ struct PacketInfo {
 };
 
 struct Status {
-  const uint32_t version = 2001065; // version year month day release
+  const uint32_t version = 2001080; // version year month day release
   bool mqtt_connected = false;
   SysInfo sysInfo;
   PacketInfo lastPacketInfo;
