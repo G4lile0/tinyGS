6ee115c418a5104812c805be5646bcf5ea068573
Paul Schulz <paul@mawsonlakes.org>
2020-01-17 01:09:40 +1030

    Updates code documentation with expended list of supported boards

--- FossaGroundStation/FossaGroundStation.ino
+++ FossaGroundStation/FossaGroundStation.ino
@@ -27,30 +27,40 @@
   boards section.
   The developers of this project have no relation with the Fossa team in
   charge of the mission, we are passionate about space and created this
-  project to be able to trackand use the satellites as well as supporting
+  project to be able to track and use the satellites as well as supporting
   the mission.
 
   Supported boards
     Heltec WiFi LoRa 32 V1 (433MHz SX1278)
     Heltec WiFi LoRa 32 V2 (433MHz SX1278)
+      https://heltec.org/project/wifi-lora-32/
     TTGO LoRa32 V1 (433MHz SX1278)
     TTGO LoRa32 V2 (433MHz SX1278)
+    T-BEAM + OLED  (433MHz SX1278)
+    ESP32 dev board + SX126X with crystal (Custom build, OLED optional)
+    ESP32 dev board + SX126X with TCXO (Custom build, OLED optional)
+    ESP32 dev board + SX127X (Custom build, OLED optional)
+    TTGO LoRa32 V2 (Manualy swaped SX1267 to SX1278)
 
-  Supported modules
+  Supported radio modules
     sx126x
     sx127x
 
-    World Map with active Ground Stations and satellite stimated possition 
-    Main community chat: https://t.me/joinchat/DmYSElZahiJGwHX6jCzB3Q
+  See: 'src/ConfigManager.cpp' for the complete list of supported boards.
 
-    In order to onfigure your Ground Station please open a private chat to get your credentials https://t.me/fossa_updates_bot
-    Data channel (station status and received packets): https://t.me/FOSSASAT_DATA
-    Test channel (simulator packets received by test groundstations): https://t.me/FOSSASAT_TEST
+  World Map with active Ground Stations and satellite stimated position
+  Main community chat: https://t.me/joinchat/DmYSElZahiJGwHX6jCzB3Q
 
-    Developers:
-      @gmag12       https://twitter.com/gmag12
-      @dev_4m1g0    https://twitter.com/dev_4m1g0
-      @g4lile0      https://twitter.com/G4lile0
+  In order to configure your Ground Station please open a private
+  chat to get your credentials https://t.me/fossa_updates_bot
+
+  Data channel (station status and received packets): https://t.me/FOSSASAT_DATA
+  Test channel (simulator packets received by test groundstations): https://t.me/FOSSASAT_TEST
+
+  Developers:
+    @gmag12       https://twitter.com/gmag12
+    @dev_4m1g0    https://twitter.com/dev_4m1g0
+    @g4lile0      https://twitter.com/G4lile0
 
 ====================================================
   IMPORTANT:
