5fa14744836e74890c18517aca552a84cae0faac
4m1g0 <dev.4m1g0@gmail.com>
2021-02-14 05:55:47 +0100

    Add exception in case there is git repository

--- git_rev_macro.py
+++ git_rev_macro.py
@@ -1,8 +1,13 @@
 import subprocess
 
-revision = (
-    subprocess.check_output(["git", "describe", "--tags", "--always"])
-    .strip()
-    .decode("utf-8")
-)
+revision = ""
+try:
+    revision = (
+        subprocess.check_output(["git", "describe", "--tags", "--always"])
+        .strip()
+        .decode("utf-8")
+    )
+except:
+    pass
+
 print("-DGIT_VERSION='\"%s\"'" % revision)
