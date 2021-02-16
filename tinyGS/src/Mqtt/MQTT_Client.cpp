cdec24ba9263f3d61f01f57ff929f82df744864c
4m1g0 <dev.4m1g0@gmail.com>
2021-02-16 10:26:51 +0100

    Rename CRC_error to crc_error to mach all other places

--- tinyGS/src/Mqtt/MQTT_Client.cpp
+++ tinyGS/src/Mqtt/MQTT_Client.cpp
@@ -165,7 +165,7 @@ void  MQTT_Client::sendRx(String packet)
   doc["unix_GS_time"] = now;
   doc["usec_time"] = (int64_t)tv.tv_usec + tv.tv_sec * 1000000ll;
   doc["time_offset"] = status.time_offset;
-  doc["CRC_error"] = status.lastPacketInfo.crc_error;
+  doc["crc_error"] = status.lastPacketInfo.crc_error;
   doc["data"] = packet.c_str();
   doc["NORAD"] = status.modeminfo.NORAD;
   doc["test"] = configManager.getTestMode();
@@ -222,7 +222,7 @@ void  MQTT_Client::sendStatus()
   doc["rssi"] = status.lastPacketInfo.rssi;
   doc["snr"] = status.lastPacketInfo.snr;
   doc["frequency_error"] = status.lastPacketInfo.frequencyerror;
-  doc["CRC_error"] = status.lastPacketInfo.crc_error;
+  doc["crc_error"] = status.lastPacketInfo.crc_error;
   doc["unix_GS_time"] = now;
   doc["usec_time"] = (int64_t)tv.tv_usec + tv.tv_sec * 1000000ll;
   doc["time_offset"] = status.time_offset;
