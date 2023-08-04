1d776f5de3760af9a1bc1631276757d7426fe361
G4lile0 <galile0@hotmail.com>
2023-08-04 16:56:52 +0200

    remove local copy of radiolib & update platform

--- tinyGS/src/Status.h
+++ tinyGS/src/Status.h
@@ -67,7 +67,7 @@ struct TextFrame {
 };
 
 struct Status {
-  const uint32_t version = 2308031; // version year month day release
+  const uint32_t version = 2308041; // version year month day release
   const char* git_version = GIT_VERSION;
   bool mqtt_connected = false;
   bool radio_ready = false;
