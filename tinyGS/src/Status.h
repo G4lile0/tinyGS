b9a112ac357b5e3257313293cbd36f81851f44d2
gargomoma <gargomoma@users.noreply.github.com>
2024-02-13 12:59:01 +0100

    Add ESP32-C3 support

--- tinyGS/src/Status.h
+++ tinyGS/src/Status.h
@@ -67,7 +67,7 @@ struct TextFrame {
 };
 
 struct Status {
-  const uint32_t version = 2402091; // version year month day release
+  const uint32_t version = 2402121; // version year month day release
   const char* git_version = GIT_VERSION;
   bool mqtt_connected = false;
   bool radio_ready = false;
